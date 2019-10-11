921fae9eebd5522c6f282c4ecf5fb958b6d36c9c
Sergii Stryzg <strizgsergey@gmail.com>
2019-10-11 15:56:57 +0300

    Updates

---
+++ precache-manifest.2a966715a7125cdb553d4390a96f6a8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38f317922e9d773207b6d9b1ef4417fc",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "87e0ddbd1c3dc8db5818",
+    "url": "/goit-react-hw-01-components/static/css/main.66612f3e.chunk.css"
+  },
+  {
+    "revision": "cb342b83b08581dc8d53",
+    "url": "/goit-react-hw-01-components/static/js/2.bda5f8fb.chunk.js"
+  },
+  {
+    "revision": "87e0ddbd1c3dc8db5818",
+    "url": "/goit-react-hw-01-components/static/js/main.d53245b1.chunk.js"
+  },
+  {
+    "revision": "4d9ce36fabe79d32c0ed",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.7a5c0bc9.js"
+  }
+]);
